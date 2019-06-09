366396a25808a86d8083bf9e5c7d29b3de1bd809
ReneeM <rnmessick@gmail.com>
2019-06-09 13:43:13 -0700

    Add multiplyArray branch

--- starter-code/app.js
+++ starter-code/app.js
@@ -112,11 +112,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+  var productMultiplyArray = multiply(multiply(multArr[0], multArr[1])[0], multArr[2])[0];
+  var multiplyArrayMessage = 'The numbers ' + multArr[0] + ',' + multArr[1] + ',' + multArr[2] + ' have a product of ' + productMultiplyArray + '.';
+  return [productMultiplyArray, multiplyArrayMessage];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
